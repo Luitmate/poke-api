c6203f05b28cbe78fc4c59c33e695fa4b0cf211b
lunov08 <lukitmate@gmail.com>
2021-12-05 09:41:08 -0300

    muestra lista de pokemones

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function mostrarListaPokemones(listaPokemones) {
+    console.log('la lista de pokemones es', listaPokemones)
+    const $lista = document.querySelector('.list-group')
+
+    for(pokemon of listaPokemones) {
+        let nuevoElementoLista = document.createElement('a')
+        nuevoElementoLista.setAttribute("href", pokemon.url)
+        nuevoElementoLista.setAttribute("class", "list-group-item list-group-item-action")
+        nuevoElementoLista.textContent = pokemon.name
+        $lista.appendChild(nuevoElementoLista)
+    }
+}
+
 
 function iniciar() {
     const POKEMON_URL = 'https://pokeapi.co/api/v2/pokemon/'
     fetch(POKEMON_URL)
-        .then(response => response.json())
-        .then(response => console.log(response))
+        .then(response => response.json())   
+        .then(response => {
+            console.log(response)
+            const listaPokemones = response.results
+            mostrarListaPokemones(listaPokemones)
+        })
 }
 
 iniciar()
+
